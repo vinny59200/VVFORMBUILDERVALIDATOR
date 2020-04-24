5ce1b9ac092d81628cb1f46ec4fc6e3f93ca2d7d
unknown <vvauban@gmail.com>
2020-04-24 02:50:25 +0200

    Add the form with cross validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup,} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'VVFORMBUILDERVALIDATOR';
+  form: FormGroup;
+  formsubmitted:boolean;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      display: this.fb.control(''),
+      mynumber: this.fb.control(49, [Validators.required, Validators.min(0), Validators.max(100)]),
+    }, { validators: customCrossValidator });
+  }
+
+  submit() {
+   this.formsubmitted=true;
+  }
+
+  get display() {
+    return this.form.get('display').value==='yes';
+  }
+
+  get mynumber() {
+    return this.form.get('mynumber');
+  }
+
+  clearSubmit(){
+    this.formsubmitted=false;
+   }
+
+
+}
+
+export function customCrossValidator(group: FormGroup) {
+  const {  display, mynumber } = group.controls;
+  if (display.value==='yes' && mynumber.value>50 ) {
+    return { customCrossValidator: true };
+  } 
+  return null;
 }
